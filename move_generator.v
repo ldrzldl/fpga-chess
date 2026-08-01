aa1f7aa78f1d7f258749500286141f4669b264f2
Rain <rain@Rainui-MacBookAir.local>
2026-08-01 18:50:55 +0900

    plan.md, move_generator.v 추가 / make_king_move_board -> king_move_generator.v로 모듈 이름 변경 및 start_pos에 king이 없을 경우 잘 작동되도록 코드 살짝 수정

---
+++ move_generator.v
@@ -0,0 +1,22 @@
+`timescale 1ps/1ns
+
+module move_generator (
+    input wire [24:0] opponent_board,
+    input wire [24:0] team_board,
+    input wire [24:0] king_board,
+    input wire [24:0] rook_board,
+    input wire [24:0] pawn_board,
+    input wire [24:0] start_pos_board,
+    output wire [24:0] move_board
+);
+
+    wire [24:0] king_move_board;
+    king_move_generator kmg(start_pos_board, king_board, team_board, king_move_board);
+    wire [24:0] rook_move_board;
+    rook_move_generator rmg(start_pos_board, rook_board, team_board, opponent_board, rook_move_board);
+    wire [24:0] pawn_move_board;
+    pawn_move_generator pmg(start_pos_board, pawn_board, team_board, opponent_board, pawn_move_board);
+
+    assign move_board = king_move_board | rook_move_board | pawn_move_board;
+
+endmodule
